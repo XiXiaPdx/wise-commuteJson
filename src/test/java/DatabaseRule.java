99039d07d5b198aef36656629d644c1d2f278440
Ryan <ryanjonesirl@gmail.com>
2017-04-05 16:26:34 -0700

    add directories and skeleton files

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,18 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wise_commute_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteUserQuery = "DELETE FROM users *;";
+      con.createQuery(deleteUserQuery).executeUpdate();
+    }
+  }
+}
