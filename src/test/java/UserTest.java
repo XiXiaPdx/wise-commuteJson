e39d6f10ae193fe9cf04d652b1a394210257e6c0
znergy <Guest@Epicodus-6E.local>
2017-04-11 13:59:28 -0700

    add user class and tests

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -14,4 +14,22 @@ public class UserTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test
+  public void save_assignsIdToObjectAndSavesObjectToDatabase() {
+    User testUser = new User("xyz@gmail.com", "xyz", "1234password", "www.imgur.com/harambe.jpg");
+    testUser.save();
+    User savedUser = User.find(testUser.getId());
+    assertEquals(testUser.getId(), savedUser.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfUser_false() {
+    User firstUser = new User("xyz@gmail.com", "xyz", "1234password", "www.imgur.com/harambe.jpg");
+    firstUser.save();
+    User secondUser = new User("xyz@gmail.com", "xyz", "1234password", "www.imgur.com/harambe.jpg");
+    secondUser.save();
+    assertEquals(firstUser.getId(), User.find(firstUser.getId()).getId());
+    assertEquals(secondUser.getId(), User.find(secondUser.getId()).getId());
+  }
+
 }
---
+++ wise_commute.sql
@@ -0,0 +1,181 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.3
+-- Dumped by pg_dump version 9.5.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: reports; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE reports (
+    id integer NOT NULL,
+    id_user integer,
+    id_train integer,
+    train_capacity character varying,
+    comment character varying(100)
+);
+
+
+ALTER TABLE reports OWNER TO "Guest";
+
+--
+-- Name: reports_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE reports_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE reports_id_seq OWNER TO "Guest";
+
+--
+-- Name: reports_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE reports_id_seq OWNED BY reports.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE users (
+    id integer NOT NULL,
+    email character varying,
+    username character varying,
+    password character varying,
+    image character varying
+);
+
+
+ALTER TABLE users OWNER TO "Guest";
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE users_id_seq OWNER TO "Guest";
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY reports ALTER COLUMN id SET DEFAULT nextval('reports_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Data for Name: reports; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY reports (id, id_user, id_train, train_capacity, comment) FROM stdin;
+\.
+
+
+--
+-- Name: reports_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('reports_id_seq', 1, false);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY users (id, email, username, password, image) FROM stdin;
+\.
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('users_id_seq', 1, false);
+
+
+--
+-- Name: reports_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY reports
+    ADD CONSTRAINT reports_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: epicodus
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM epicodus;
+GRANT ALL ON SCHEMA public TO epicodus;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
